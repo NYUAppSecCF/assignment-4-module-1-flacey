7bea440aab6579f0b0c4626ec687fa9f2d322a63
Frank Lacey <fcl6047@nyu.edu>
2026-04-28 07:01:17 -0400

    Fix typo after removal of overly permissive code

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
@@ -11,6 +11,6 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Reporter(private val token: String) : {
+class Reporter(private val token: String) {
     
 }
